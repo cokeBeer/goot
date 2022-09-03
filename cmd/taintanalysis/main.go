817557e9a2e9319c1ed9212a90484414fc386f9f
cokeBeer <elevenbladeknife@gmail.com>
2022-09-03 21:55:57 +0800

    update README.md

--- cmd/taintanalysis/main.go
+++ cmd/taintanalysis/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/cokeBeer/goot/pkg/example/taint"
 
 func main() {
-	pkg := taint.Gostd
-	runner := taint.NewRunner(pkg...)
+	//pkg := taint.Gostd
+	runner := taint.NewRunner("fmt...")
 	runner.SrcPath = "passthrough.json"
 	runner.DstPath = "passthrough.json"
 	runner.Debug = true
